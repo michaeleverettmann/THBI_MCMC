d09bcfb87e611ca55a4d6ae2c74ca2855e9a8452
brennanbrunsvik <brennan.brunsvik@gmail.com>
2021-09-22 15:27:35 -0700

    Progress sending files to ERI computers automatically
    Now on either mac or ERI computers, I should be able to run transfer./compileTest_ALL.bash and it will go to propmat, cadmineos and do the compilations and tests. There might be a transfer error occasionally, as PropMat didn't compile the first time I did this and I had to go to the PropMat folder and run ./sendERI.bash from there to get files over properly. Not sure why. I will continue to figure it out...
    
    First, should be able to go to the THBI transfer folder and run sendERI_ALL.bash to send everything to the proper location on ERI computers.

--- transfer/compileTest_ALL.bash
+++ transfer/compileTest_ALL.bash
@@ -1,7 +1,20 @@
-transferFolds=\
+#!/bin/bash 
+# This script loops through several folders, compiles the code, and tests the compilation success. 
+# It checks which computer you are on (by looking at where your home directory is). 
+# Then you might have different locations where code is stored depending on the different computers. 
+homeDir=~ 
+
+if [ $homeDir = '/Users/brennanbrunsvik' ]; then # My mac computer. Enter relevant folders here. 
+    transferFolds=\
 '/Users/brennanbrunsvik/Documents/UCSB/ENAM/THBI_ENAM/transfer
 /Users/brennanbrunsvik/Documents/repositories/Peoples_codes/CADMINEOS/transfer
 /Users/brennanbrunsvik/Documents/repositories/Peoples_codes/PropMat/transfer'
+elif [ $homeDir = '/home/brunsvik' ]; then # ERI computers. Enter relevant folders here. 
+    transferFolds=\
+'/home/brunsvik/Documents/UCSB/ENAM/THBI_ENAM/transfer
+/home/brunsvik/Documents/repositories/Peoples_codes/CADMINEOS/transfer
+/home/brunsvik/Documents/repositories/Peoples_codes/PropMat/transfer'
+fi 
 
 for fold in $transferFolds
 do 
