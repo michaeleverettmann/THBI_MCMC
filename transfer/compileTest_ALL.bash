75ff46eec8e1f3a93a2c839a27ef641cc4cd6ec9
brennanbrunsvik <brennan.brunsvik@gmail.com>
2021-09-23 12:34:48 -0700

    Transfer scripts progress.

--- transfer/compileTest_ALL.bash
+++ transfer/compileTest_ALL.bash
@@ -4,16 +4,19 @@
 # Then you might have different locations where code is stored depending on the different computers. 
 homeDir=~ 
 
+# TODO much easier if we just use ~/stuff. I didn't realize that my path structure is the same on the mac and linux computers after home. 
 if [ $homeDir = '/Users/brennanbrunsvik' ]; then # My mac computer. Enter relevant folders here. 
     transferFolds=\
 '/Users/brennanbrunsvik/Documents/UCSB/ENAM/THBI_ENAM/transfer
 /Users/brennanbrunsvik/Documents/repositories/Peoples_codes/CADMINEOS/transfer
-/Users/brennanbrunsvik/Documents/repositories/Peoples_codes/PropMat/transfer'
+/Users/brennanbrunsvik/Documents/repositories/Peoples_codes/PropMat/transfer
+/Users/brennanbrunsvik/MATLAB/transfer'
 elif [ $homeDir = '/home/brunsvik' ]; then # ERI computers. Enter relevant folders here. 
     transferFolds=\
 '/home/brunsvik/Documents/UCSB/ENAM/THBI_ENAM/transfer
 /home/brunsvik/Documents/repositories/Peoples_codes/CADMINEOS/transfer
-/home/brunsvik/Documents/repositories/Peoples_codes/PropMat/transfer'
+/home/brunsvik/Documents/repositories/Peoples_codes/PropMat/transfer
+/home/brunsvik/MATLAB/transfer'
 fi 
 
 for fold in $transferFolds
--- transfer/sendERI_ALL.bash
+++ transfer/sendERI_ALL.bash
@@ -2,7 +2,8 @@
 transferFolds=\
 '/Users/brennanbrunsvik/Documents/UCSB/ENAM/THBI_ENAM/transfer
 /Users/brennanbrunsvik/Documents/repositories/Peoples_codes/CADMINEOS/transfer
-/Users/brennanbrunsvik/Documents/repositories/Peoples_codes/PropMat/transfer'
+/Users/brennanbrunsvik/Documents/repositories/Peoples_codes/PropMat/transfer
+/Users/brennanbrunsvik/MATLAB/transfer'
 
 for fold in $transferFolds
 do 
