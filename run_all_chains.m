ff28f2a898bb16e213f5892712cc75bc7fc0502a
brennanbrunsvik <brennan.brunsvik@gmail.com>
2022-06-28 16:20:37 -0700

    Plot shallow model, up to 15 km, as heatmap. Prep for including sediments.

--- run_all_chains.m
+++ run_all_chains.m
@@ -272,6 +272,8 @@ fprintf('  > Plotting model suite\n')
 [ suite_of_models ] = c3_BUILD_MODEL_SUITE(allmodels_collated,par );
 plot_SUITE_of_MODELS( suite_of_models,posterior,1,[resdir,'/suite_of_models.png'],[par.data.stadeets.Latitude,par.data.stadeets.Longitude]);
 plot_HEATMAP_ALLMODELS(suite_of_models,par,1,[resdir,'/heatmap_of_models.pdf']);
+plot_HEATMAP_ALLMODELS_shallow(suite_of_models,par,1,[resdir_fig,'/heatmap_of_models_shallow.pdf']);
+
 
 %% Save some things
 fprintf('  > Saving misfits, allmods, posterior, model suite\n')
