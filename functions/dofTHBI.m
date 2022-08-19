cc48ca8b65144f88fe643a6532db412acc817ce3
brennanbrunsvik <brennan.brunsvik@gmail.com>
2022-08-19 15:28:48 -0700

    TEMPORARY BUT IMPORTANT Degrees of freedom for Sp ccp totally changes results.
    I'm now doing what we were doing for the Sp receiver functions (not ccp) to estimate degrees of freedom. I get about 3x as many degrees of freedom (something like from 17 to 50). The change makes it so we can fit (noisy) synthetic data, and get all the MLDs and stuff I put in the synthetic model. Before, I was not recovering the MLDs.

--- functions/dofTHBI.m
+++ functions/dofTHBI.m
@@ -18,7 +18,10 @@ for id = 1:length(dtypes)
             trudata.(dtype)(itr).dof = trudata.(dtype)(itr).Nobs;
         elseif any(strcmp({'BW','RF'},pdt{1}))
             if strcmp(pdt{3},'ccp')
-                trudata.(dtype)(itr).dof = length(trudata.(dtype)(itr).zz)*trudata.(dtype)(itr).dof_per_z;
+%                 trudata.(dtype)(itr).dof = length(trudata.(dtype)(itr).zz)*trudata.(dtype)(itr).dof_per_z;
+                warning('CHANGING DEGREES OF FREEDOM FOR CCP'); 
+                trudata.(dtype)(itr).dof = mean([scdofcalc(trudata.(dtype)(itr).PSV(:,1)),...
+                       scdofcalc(trudata.(dtype)(itr).PSV(:,2))]);
             else
                 trudata.(dtype)(itr).dof = mean([scdofcalc(trudata.(dtype)(itr).PSV(:,1)),...
                                scdofcalc(trudata.(dtype)(itr).PSV(:,2))]);
