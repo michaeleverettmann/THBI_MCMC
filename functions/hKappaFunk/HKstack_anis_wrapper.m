192740322893ab1010722afa4f4f4bc21ed8f4a4
brennanbrunsvik <brennan.brunsvik@gmail.com>
2022-11-22 14:37:14 -0800

    Deleted old version of HK anis code, now just have hk_anis repository in use I think.

--- functions/hKappaFunk/HKstack_anis_wrapper.m
+++ functions/hKappaFunk/HKstack_anis_wrapper.m
@@ -17,123 +17,32 @@ function [HK_A, HK_H, HK_K, t_pred] = HKstack_anis_wrapper(par, model, ...
 
 % MCMC/THBI wrapper to get relevant model/data and use it for radially 
 % anisotropic HK stack. 
+%brb2022.11.22 Modifying this to use my hk_anis repository. 
 
-%%% Use hk_anis repository here. 
-% Get average crustal values to be consistent with Zhu and Kanamori math
+% Extract anisotropy values from the model. 
 if isempty(options.posterior); % Have to calculate averages from 1-D profile. 
-    xi = model.Sanis/100+1; % Radial S anisotropy. add one because for some reason here anisotropy is + or -
+    xi = model.Sanis/100+1; % Radial S anisotropy. 
     phi = model.Panis/100+1; % Radial P anisotropy
 else % If we have the posterior, get average values from there. 
     posterior = options.posterior; 
     xi = median(posterior.xicrust); 
     phi = median(posterior.phicrust); 
+    % If there is an error about xi or phi being a scalar in stead of vector, maybe this is where things are going wrong. 
 end
 eta = ones(size(model.Sanis)); % TODO if introduce non 1 eta, need to change this!!!
 
+% Get average crustal values to be consistent with Zhu and Kanamori math. This also uses earth flattening transformation. 
 [rhoav, vsav, vpav, xiav, phiav, etaav] =  hk_average_crust(...
     model.rho, model.VS, model.VP, ...
     xi, phi, eta, ...
     model.z, model.zmoh);  
 
-% Receiver functions making HK stack for each rf? Or just 1? 
+% Do HK stack on one receiver function. 
 [HK_A, HK_H, HK_K, t_pred] = hk_anis(...
     RF, tt, rayp, vsav, rhoav, xiav, phiav, etaav,...
     'kNum', options.kNum, 'hNum', options.hNum,...
     'kBounds', options.kBounds, 'hBounds', options.hBounds,...
     'phase_wts', options.phase_wts); 
-%%%
-% % % % 
-% % % % % % % % % % Earth flattening transformation
-% % % % a = 6371; 
-% % % % zflt = @(z)-a .* log((a-z)./a); 
-% % % % vflt = @(z,v)a./(a-z) .* v; 
-% % % % % r = a - model.z; 
-% % % % % zzf = -a .* log(r./a); 
-% % % % % vsf = a./r .* vs; 
-% % % % % vpf = a./r .* vp; 
-% % % % % zz = zzf; 
-% % % % % vs = vsf; 
-% % % % % vp = vpf; 
-% % % % model.z_orig = model.z; 
-% % % % model.zmoh_orig = model.zmoh; 
-% % % % model.z    = zflt(model.z   ); 
-% % % % model.zmoh = zflt(model.zmoh); 
-% % % % model.VS = vflt(model.z_orig, model.VS); 
-% % % % model.VP = vflt(model.z_orig, model.VP); 
-% % % % 
-% % % % % Choose weighting for different phases. There are a few valid choices.
-% % % % phase_wts = options.phase_wts; 
-% % % % 
-% % % % % Get average crust velocity. There are again a few choices. 
-% % % % % Get vsv, vpv. If there is anisotropy, this influences HK calculation.
-% % % % isCrust = model.z <= model.zmoh; 
-% % % % isCrustF = find(isCrust); 
-% % % % if model.z(isCrustF(end-1)) == model.z(isCrustF(end)); % Handle the duplicated moho depth for crust and mantle
-% % % %     isCrustF(end) = []; % Remove mantle value
-% % % %     isCrust(:) = false; 
-% % % %     isCrust(isCrustF) = true; 
-% % % % end
-% % % % dz = zeros(size(model.z)); % For weighted mean, integral style. 
-% % % % dz(1:end-1,1) = dz(1:end-1,1) + 0.5 .* diff(model.z);
-% % % % dz(2:end  ,1) = dz(2:end  ,1) + 0.5 .* diff(model.z); 
-% % % % dzC = dz(isCrust); 
-% % % % 
-% % % % % Function to calculate mean accounting for variable dz spacing. Simple integral mean. 
-% % % % meanInt = @(m,dz)sum(dz.*m)/sum(dz); 
-% % % % % vrAv = @(m,dz)( (1./meanInt(1./m,dz) + meanInt(m,dz) ) / 2 ); % Voigt Reuss Hill average (or whatever it's called). 
-% % % % vrAv = @(m,dz)( (1./meanInt(1./m,dz) ) ); % This averaging approach is slightly more consistent with propmat than the voigt reuss. Makes sense... it's all about travel times. 
-% % % % 
-% % % % rho = meanInt(model.rho(isCrust), dzC); 
-% % % % eta = 1; % IMPORTANT if eta is ever not 1, we need to modify the code. 
-% % % % vsAv = vrAv(model.VS(isCrust), dzC); 
-% % % % vpAv = vrAv(model.VP(isCrust), dzC); 
-% % % % 
-% % % %  % TODO need to incocporate sediment vpvs and xi into the averages properly. Tough to decide how to balance them being model parameters that we plot and look at, while HK sampling should be based on different parameters.  brb 2022.08.31, I cant really tell the difference between hk stacks or the positions of plotted points when changing between these two values. 
-% % % % vpvs_av_iso = vpAv / vsAv; % including in sediment. 
-% % % % 
-% % % % if isempty(options.posterior); % Have to calculate averages from 1-D profile. 
-% % % %     xi = model.Sanis(isCrust)/100+1; % Radial S anisotropy. add one because for some reason here anisotropy is + or -
-% % % %     xi = vrAv(xi, dzC); 
-% % % %     phi = model.Panis(isCrust)/100+1; % Radial P anisotropy
-% % % %     phi = vrAv(phi, dzC); 
-% % % % else % If we have the posterior, get average values from there. 
-% % % %     posterior = options.posterior; 
-% % % %     xi = median(posterior.xicrust); 
-% % % %     phi = median(posterior.phicrust); 
-% % % % end
-% % % %     
-% % % % 
-% % % % % % %%brb2022.11.16 Old version of HK xi correction, where I only solved Christoffel equations for the vs and vp value in our model. Quicker. 
-% % % % % % % % Assume ray path change is insignificant when adding anisotropy
-% % % % % % % incAngVs = rayp2inc(rayp/111.1949, vsAv              ); 
-% % % % % % % incAngVp = rayp2inc(rayp/111.1949, vsAv * vpvs_av_iso);
-% % % % % % % 
-% % % % % % % [velVs,~] = christof_radial_anis(vsAv, vsAv * vpvs_av_iso,...
-% % % % % % %     xi, phi, eta, rho, incAngVs); % Velocities for ray with vs incidence angle
-% % % % % % % [velVp,~] = christof_radial_anis(vsAv, vsAv * vpvs_av_iso,...
-% % % % % % %     xi, phi, eta, rho, incAngVp); % Velocities for ray with vp incidence angle
-% % % % % % % vsvAn = velVs(2); % Vsv for s ray 
-% % % % % % % vpAn  = velVp(1); % Vp for p ray
-% % % % % % % 
-% % % % % % % % fprintf('\nvsvAn = %1.5f : vpAn = %1.5f\n', vsvAn, vpAn)
-% % % % % % % 
-% % % % % % % vpvsEffective = vpAn/vsvAn; 
-% % % % % % % 
-% % % % % % % [HK_A, HK_H, HK_K, t_pred] = HKstack(RF, tt, ...
-% % % % % % %     rayp/111.1949, phase_wts, ...
-% % % % % % %     vsAv, ... % isotropic
-% % % % % % %     linspace(options.hBounds(1), options.hBounds(2), options.hNum)',...
-% % % % % % %     linspace(options.kBounds(1), options.kBounds(2), options.kNum), ...
-% % % % % % %     'vpvsANIS_vpvs',vpvsEffective/vpvs_av_iso,'Vsv_av',vsvAn); 
-% % % % % % %%brb2022.11.16 END 
-% % % % [HK_A, HK_H, HK_K, t_pred] = hk_anis(...
-% % % %     RF, tt, rayp, vsAv, rho, xi, phi, eta,...
-% % % %     'kNum', options.kNum, 'hNum', options.hNum,...
-% % % %     'kBounds', options.kBounds, 'hBounds', options.hBounds,...
-% % % %     'phase_wts', options.phase_wts); 
-% % % % % fhand=@()hk_anis(...
-% % % % %     RF, tt, rayp, vsAv, rho, xi, phi, eta); 
-% % % % % timeit(fhand)
 
 % The rest of the script expects transposed hk stack. For IRIS ears data loaded from Zach's functions, K is first dimension, H is second bb2021.12.31
 HK_A = HK_A'; 
