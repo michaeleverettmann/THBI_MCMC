0a547bd4347dd09940e1ecc79fe93e25ee5f1e91
brennanbrunsvik <46268696+brennanbrunsvik@users.noreply.github.com>
2024-02-06 13:04:30 -0800

    A python script and function that will handle telewavesim
    Next step will be a matlab wrapper, which handles all conversion of inputs from the propmat style to telewavesim style, and handles calling the python function.

---
+++ matlab_to_telewavesim/run_telewavesim.py
@@ -0,0 +1,50 @@
+from obspy.core import Stream
+from obspy.signal.rotate import rotate_ne_rt
+from telewavesim import utils as ut
+from telewavesim import wiggle as wg
+import numpy as np 
+
+# LAYmodel,ID,ph,samprate,inc,synthperiod,nsamps,cutf,sourc
+def run_telewavesim(modfile, wvtype, npts, dt, dp, use_obs, c, rhof, slow, baz): 
+    model = ut.read_model(modfile)
+    trxyz = ut.run_plane(model, slow, npts, dt, baz=baz, wvtype=wvtype,
+                        obs=use_obs, dp=dp, c=c, rhof=rhof)
+
+    ntr = trxyz[0] # North component
+    etr = trxyz[1] # East component
+    ztr = trxyz[2] # Vertical component
+
+    # Copy to radial and transverse. Temporary: Will do rotation. 
+    rtr = ntr.copy() # Radial component
+    ttr = etr.copy() # Transverse component
+
+    # Rotate to radial and transverse
+    if not ((baz % 360) == 0):
+        rtr.data, ttr.data = rotate_ne_rt(ntr.data, etr.data, baz)
+
+    traces = np.array([rtr, ttr, ztr]) # TODO double check. do we want radial transverse? 
+    tt = np.arange(0, npts, dt) # TODO double check. 
+    status = ''
+    cmdout = '' 
+    return traces, tt, status, cmdout
+
+
+# Test the function
+if __name__ == '__main__': 
+    modfile = './demo.txt'
+    wvtype = 'P'
+
+    npts = 3000 # Number of samples
+    dt = 0.01   # Sample distance in seconds
+
+    dp = 2000. # Deployment depth below sea level in meters
+    # use_obs = dp > 0 # Only use OBS code if deployment depth is beneath sea level. 
+    use_obs = True
+
+    c = 1.5      # P-wave velocity in salt water (km/s)
+    rhof = 1027. # Density of salt water (kg/m^3)
+
+    slow = 0.06 # Horizontal slowness (or ray parameter) in s/km 
+    baz = 0.    # Back-azimuth direction in degrees (has no influence if model is isotropic)
+
+    run_telewavesim(modfile, wvtype, npts, dt, dp, use_obs, c, rhof, slow, baz)
