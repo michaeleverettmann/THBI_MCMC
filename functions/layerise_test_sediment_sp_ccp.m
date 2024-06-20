e6f46e05ddb8535a5ba272187a4d4d19098f9d36
brennanbrunsvik <46268696+brennanbrunsvik@users.noreply.github.com>
2024-06-20 10:28:55 -0700

    Make readme look pretty
    using Chatgpt :)

---
+++ functions/layerise_test_sediment_sp_ccp.m
@@ -0,0 +1,199 @@
+function [zlayt,zlayb,vlay,varargout] = layerise(Z,V,dvmin,ifplot,varargin)
+
+% This was a test of removing(?) sediment for Sp receiver functions. I
+% think it will not be in use now, but the function is kept in case the
+% test is redone. brb~2022
+
+% Code to take a continuous series of velocity (V) with depth (Z) and
+% convert it to several layers of constant velocity. 
+% New approach (brb2022.06.29) is to traverse down the model and make a new
+% layer when either the velocity or depth (or norm of those two) has
+% changed suffiently. 
+% 
+% INPUTS:
+%   Z     - [N x 1] vector of depths (km)
+%   V     - [N x 1] vector of velocities (km/s)
+%   dvmin - brb2022.06.29 removed temporarily. 
+%           Tunable variable specifying layer thickness. If this value is
+%           smaller, the continuous profile will be better fit, but with
+%           more layers. If the value is larger, the fit will be poorer,
+%           but there will be fewer layers. Recommended value: 0.1 (km/s)
+%   ifplot - option to plot (true) or not (false)
+%   varargin - any number of [N x 1] vectors for other parameters (e.g. Vp, 
+%           rho) to be discretised onto the same mesh. The discontinuity 
+%           depths will be well-preserved, but because the algorithm isn't
+%           actually adapting to these parameters, the jumps across the
+%           discontinuities will be less precisely recovered. 
+% 
+% OUTPUTS:
+%   zlayt - [Nlay x 1] vector of depths of layer tops (km)
+%   zlayb - [Nlay x 1] vector of depths of layer bases (km)
+%   vlay  - [Nlay x 1] vector of velocity values for each layer (km/s)
+%   varargout - any number of [Nlay x 1] vector of other parameter values
+%           in each layer
+
+%     Z. Eilon 08/2016
+%     Modified brb2022.06.29. 
+
+% ifplot = 1; warning('if plot force true'); 
+
+% Decide on layerise resolution. 
+% Note: The new model will not get finer than the provided Z array. 
+if isempty(dvmin); 
+    break_v                  = 0.15 ; % Tend toward break into a layer every this much velocity change. 
+    break_z                  = 10   ; % Tend toward making all layers smaller than this. Maybe MORE important than break_v: if this is too large, individual propmat pulses from seperate layers will not "touch" each other, and a velocity gradient will become distinct pulses. Per my testing, it seems that about 10 km spacing allows propmat with 4 s synthetic pulse to work properly with "touching" pulses that convey velocity gradients properly. 
+else; 
+    break_v                  = dvmin; 
+    break_z                  = 10 / 0.15 * break_v; % Don't want to provide break_z as it's own argument. Just scale this from break_v. 
+    if break_z > 30; break_z = 30; end % But cap break_z between reasonable values. 
+    if break_z < 3 ; break_z = 3 ; end
+end
+break_grad                   = 1    ; % If gradient in v between two provided depths is greater than this, divide a layer here. Mostly this value is irrelevant, we just identify grad==inf at Moho, Sediment, or other discontinuities. 
+
+if nargin<4 || isempty(ifplot)
+    ifplot=false;
+end
+
+dz = diff(Z);
+dv = diff(V);
+dvdz = dv./dz; % Gradient. 
+is_disc = find(isinf(dvdz)); % Top index of discontinuities. dz = 0. 
+
+% Loop to find the depths where we will break up layers. 
+% Put in a layer where there is strong gradients. 
+% Or if we haven't put in a layer for a long distance. 
+% Or a combination of the two. 
+% Manually assign first and last Z as layer breaks. 
+break_inds = [1]; % Indecies where we will break into layers. 
+for iz = 2:(length(Z)-1);
+% % % 
+    %!%!%!
+%     global ignore_depth
+    ignore_depth = 5; 
+%     ifplot = 1; warning('if plot force true'); 
+    if sum(dz(1:iz)) < ignore_depth; 
+%         warning('Excluding shallow layers'); 
+        continue; 
+    end
+    %!%!%!
+    
+    if dvdz(iz) > break_grad; % Make sure discontinuities (e.g. grad == inf) have a layer break. 
+        break_inds = [break_inds; iz]; 
+        continue; 
+    end 
+    
+    vchange = abs(V(iz) - V(break_inds(end))); 
+    zchange = Z(iz)     - Z(break_inds(end) ); 
+    
+    tendchange = sqrt( (vchange / break_v)^2 ...
+                      +(zchange / break_z)^2); % Will be 1 if vchange or zchange meets our predefined values of when to break layers, but also will be one if a combination of v and z change is high enough. 
+    
+    if tendchange > 1; 
+        break_inds = [break_inds; iz]; 
+    end
+end
+break_inds = unique([break_inds; length(Z)]); % Manually assign last Z as a layer break. 
+
+zlayt = Z(break_inds(1:end-1)); % Top depth of each layer. 
+zlayb = Z(break_inds(2:end  )); % Bottom depth of each layer. 
+
+dup_vals = (zlayt == zlayb); % This can catch duplicate depths in one array - Remove accidentally added infinitesimally small layers. 
+zlayt = zlayt(~dup_vals); 
+zlayb = zlayb(~dup_vals); 
+
+% Look for some obvious signs of mistakes.  
+layer_gaps = zlayt(2:end) ~= zlayb(1:end-1); 
+if any(layer_gaps); 
+    error('The bottom of some layer is not the same as the top of the next layer'); 
+elseif Z(end) ~= zlayb(end);
+    error('Bottom of new layers not equal to bottom of old layers'); 
+elseif Z(1) ~= zlayt(1); 
+    error('Top of new layers not equal to top of old layers'); 
+end
+
+%%% Get ready to take average of velocity and whatever within each layer. 
+nlay               = length(zlayt); % Number of layers in new model
+vlay               = nan(size(zlayt)); % Output velocity
+
+% Keep track of whether above or below layer boundary. For discontinuities. 
+Z_shift            = Z; 
+Z_shift(is_disc  ) = Z_shift(is_disc  ) - 0.00001; 
+Z_shift(is_disc+1) = Z_shift(is_disc+1) + 0.00001; 
+
+% Prepare varagin/out for processing.
+process_varargin = ~isempty(varargin);  
+if process_varargin; % Prepare to process varargin, if it was provided. If needed for testing: varargin = [{V+1} {V+2}]; 
+    nargs = length(varargin); 
+    varargin_arr = nan(size(Z,1), nargs); % Make into an array for vectorized processing    
+    for iv = 1:nargs; 
+        varargin_arr(:,iv) = varargin{iv}; 
+    end
+    varargout_arr = nan(nlay, nargs); 
+else
+    varargout = {}; 
+end
+
+% Important loop - get average of v and varargin within each layer. If needed for testing: disp([Z(in_lay)' ; V(in_lay)']); % See what depths and velocities you are taking. 
+for iz = 1:length(zlayt); 
+    %!%!%! TEMPORARY 
+    in_lay = and( ( Z_shift >= zlayt(iz) ) , ( Z_shift <= zlayb(iz) ) ); % Within this layer or not? Note: This realies on Z_shift having discontinuity depths modified. 
+%     global continue_up 
+    continue_up = true;  
+    if continue_up; 
+        if iz <3; 
+            in_lay = find(in_lay); 
+            in_lay = in_lay(end); 
+        end 
+    end
+    %!%!%! Temporary @ 
+    vlay(iz) = mean(V(in_lay)); % Mean of velocity from top to bottom of this layer. 
+    if process_varargin; 
+        varargout_arr(iz, :) = mean(varargin_arr(in_lay,:),1); % Mean of all varargin varaibles, from top to bottom of this layer. Take mean along first dimension.  
+    end
+end
+
+if any(isnan(vlay)); 
+    error('Did not get velocity for some layer. '); 
+end
+
+% Convert varargout_arr to an array of cells. 
+if process_varargin; 
+    if any(isnan(varargout_arr)); 
+        error('A varargout value is nan. Might not have been averaged or provided correctly?'); 
+    end
+    
+    varargout = {}; 
+    for iarg = 1:nargs; 
+        varargout(:,end+1) = {varargout_arr(:,iarg)}; 
+    end
+end
+         
+
+if ifplot
+    figure(11); clf; set(gcf,'pos', [-581 247 514 796], 'color', 'white'); 
+    h = tiledlayout(1, 1+length(varargin),'TileSpacing','compact'); 
+    nexttile; hold on; box on; 
+
+    plot(V,Z,'-ko')
+    zlayp = reshape([zlayt';zlayb'],2*nlay,1);
+    vlayp = reshape([vlay';vlay'],2*nlay,1);
+    plot(vlayp,zlayp,'-ro')
+    set(gca,'ydir','reverse',...
+        'ylim',[0, max(Z)],'xlim',[0.9*min(V) 1.1*max(V)])
+    
+    text(min(V), 250, sprintf('nlay=%1.0f\nbreak z=%1.1f\nbreak v=%1.3f',...
+        nlay,break_z,break_v), 'fontsize', 12); 
+    
+    % other vars
+    for iv = 1:length(varargin)
+        nexttile; hold on; box on; 
+        plot(varargin{iv},Z,'-ko')
+        ivlayp = reshape([varargout{iv}';varargout{iv}'],2*nlay,1);
+        plot(ivlayp,zlayp,'-ro')
+        set(gca,'ydir','reverse','ylim',[0, max(Z)],...
+            'xlim',[0.9*min(varargin{iv}) 1.1*max(varargin{iv})])    
+    end
+end
+
+end % End function
+    
--- readme.md
+++ readme.md
@@ -1,34 +1,52 @@
-This code does a joint inversion of receiver functions and surface wave data using a Markov Chain Monte Carlo inversion. You can run a synthetic joint inversion on your computer by running example_github/RUN_example.m. Once you have a synthetic inversion running, you can adapt example_github/parms/bayes_inv_parms.m to your own needs and data. 
+# Joint Inversion of Receiver Functions and Surface Waves Using Markov Chain Monte Carlo
 
-This repository was used to run the inversion in Brunsvik et al. (2024). The code is adapted from Eilon et al. (2018). See also Golos et al. (2024), Petruska and Eilon (2022). See: Brunsvik, B., Eilon, Z., & Lynner, C. (2024). Plate‐scale imaging of eastern US reveals ancient and ongoing continental deformation. Geophysical Research Letters, 51(12), e2024GL109041.
+This code performs a joint inversion of receiver functions and surface wave data using a Markov Chain Monte Carlo (MCMC) inversion. This is a Trans-dimensional, Hierarchical, Bayesian Inversion (THBI). 
 
-There are dependencies for forward modelling. What you need depends on which data you will use. We are working to make these available as well in Github, but email me for any non-public codes you need.
-- Mineos for surface wave phase velocity. Email me for this. As of 20240612 I am working to use the Josh Russell version of Mineos, and I will update the MCMC repo once this is done: https://github.com/jbrussell/MINEOS_synthetics.  
-- Tanimoto HV code for surface wave ellipticity. We are working to make this public.  
-- Propmat for receiver functions: https://github.com/brennanbrunsvik/PropMat
-- We partially set up MCMC to work with telewavesim: https://github.com/paudetseis/Telewavesim. If you want to use this instead of Propmat, you will need solve a few more minor errors. There is an option to use telewavesim instead of propmat, but you should be fine to just use propmat. 
-- The anisotropic HK stack repository is available if you want HK stacks. https://github.com/brennanbrunsvik/hk_anis
+This code accompanies:
+- Brunsvik, B., Eilon, Z., & Lynner, C. (2024). Plate‐scale imaging of eastern US reveals ancient and ongoing continental deformation. *Geophysical Research Letters, 51*(12), e2024GL109041.
+- The code was modified from Eilon et al. (2018). See also Golos et al. (2024) and Petruska and Eilon (2022).
 
-Some other dependencies. You may not need some of these, depending on what you are running. I placed each of these in my ~/MATLAB/ folder (see a0_STARTUP_BAYES.m). You can email me and I will send the whole set of dependencies. 
-- borders. https://www.mathworks.com/matlabcentral/fileexchange/50390-borders 
-- brewermap. https://github.com/DrosteEffect/BrewerMap https://www.mathworks.com/matlabcentral/fileexchange/45208-colorbrewer-attractive-and-distinctive-colormaps 
-- fastBSpline. https://www.mathworks.com/matlabcentral/fileexchange/32509-fast-b-spline-class
-- Seizmo. Use for the 1-D velocity models, and for Tau-p. https://github.com/g2e/seizmo 
-- seis_tools-master. Get most recent version from Brennan. 
-- EilonmyFUNCTIONS. Get most recent version from Brennan. 
+## Running the example synthetic joint inversion
 
-Some steps: 
-- Change your paths in a0_STARTUP_BAYES.m. Obtain any dependencies. 
-- To speed up splines, go to your fastBSpline folder. Run the compile mex code. 
-- Run the synthetic test in example_github. 
-- For working with real data, modify "matguts" to load data from your computer. The "ENAM" folder was used for Brunsvik et al. 2024. Use it as a starting place. 
--- There is a step for downloading station information. I don't remember it, but I think it had to do with evdata1_database.m. Add this information to the readme if you find out. 
-- The "transfer" folder migrates code from your work machine to HPC. 
-- Modify the "ENAM/batch" code to work with your HPC. This has Slurm code for submitting jobs to Linux clusters at UCSB. 
-- retrieve_results_example shows how I get results back from the HPC and do some formatting. I ran this from an external drive. Should have ~100 Mb/station.  
-- 3d_models takes results from the many stations and does a smoothed inversion for a 3D model that matches the stations model PDFs. 
+1. Run the example synthetic joint inversion by executing `example_github/RUN_example.m`.
+2. Once the synthetic inversion is running, you can modify the eastern US example, applied to real data, found in the `ENAM` folder.
 
-Some behaviors to expect: 
-- In order to speed up IO on systems with many cores (e.g. 40 cores), we read and write to a folder in the ram directly for calling some fortran codes. A folder called "ramDriveTHBI" should mount automatically. 
-- update_bayes_inv_parms.m will change the parameters of your inversion depending on the stamp name. I recommend using this to develop any custom tests. 
-- run_one_chain.m is obnoxiously complicated because it needs to deal with Mineos failing. Do much testing if you modify this. 
+## Dependencies
+
+### Required depending on your data types
+
+- **Surface wave phase velocity**: Mineos (contact author for access). Working on integration with Josh Russell's public version: [Mineos_synthetics](https://github.com/jbrussell/MINEOS_synthetics).
+- **Surface wave ellipticity**: HV code from Tanimoto and Tsuboi (2009, GRL). Public release pending.
+- **Receiver functions**: [PropMat](https://github.com/brennanbrunsvik/PropMat). Alternatively, [Telewavesim](https://github.com/paudetseis/Telewavesim) is partially supported.
+- **$H-\kappa$ stacks**, including anisotropy: [hk_anis](https://github.com/brennanbrunsvik/hk_anis).
+
+### Other dependencies
+
+These are typically placed in the `~/MATLAB/` folder. Refer to `a0_STARTUP_BAYES.m` for setup details.
+
+- [borders](https://www.mathworks.com/matlabcentral/fileexchange/50390-borders)
+- [brewermap](https://github.com/DrosteEffect/BrewerMap)
+- [fastBSpline](https://www.mathworks.com/matlabcentral/fileexchange/32509-fast-b-spline-class)
+- [Seizmo](https://github.com/g2e/seizmo) (for 1-D velocity models and Tau-p)
+- **seis_tools-master**: Obtain the most recent version from Brennan.
+- **EilonmyFUNCTIONS**: Obtain the most recent version from Brennan.
+
+## Setup steps
+
+1. Update paths in `a0_STARTUP_BAYES.m` and obtain necessary dependencies.
+1. Compile mex code in the `fastBSpline` folder to speed up spline calculations (optional).
+1. Run the synthetic test in the `example_github` folder.
+1. For real data applications, use the `ENAM` folder as a starting point. Modify "matguts" to load your data from your computer. 
+1. For station information, refer to `evdata1_database.m` (add details to the readme if found).
+1. Use the "transfer" folder to migrate code from your work machine to HPC. 
+1. Modify `ENAM/batch` code (Slurm scripts for UCSB clusters) for your HPC setup.
+1. Use `retrieve_results_example` to get results from the HPC and format them (expect ~100 Mb/station).
+1. The `3d_models` folder helps create a smoothed 3D model from the station model PDFs.
+
+## Expected behaviors
+
+- IO optimization: For systems with many cores, IO operations need a folder in RAM ("ramDriveTHBI") for efficiency. This should mount automatically.  
+- Parameter updates: `update_bayes_inv_parms.m` will modify your inversion parameters depending on your STAMP. 
+- Running chains: `run_one_chain.m` is complex because it handles many cases where Mineos fails. Test thoroughly if modified.
+
+For any further assistance or to obtain dependencies, feel free to contact the author.
