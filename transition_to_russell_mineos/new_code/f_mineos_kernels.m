25fa7f5cfab11da4510dec3346d8958e952aff96
brennanbrunsvik <46268696+brennanbrunsvik@users.noreply.github.com>
2024-06-21 15:27:49 -0700

    Progress with Love waves, making kernels, and using correct frequencies.
    But the kernels come out with wrong units and don't work in b3_FORWARD_MODEL_SW_kernel

--- transition_to_russell_mineos/new_code/f_mineos_kernels.m
+++ transition_to_russell_mineos/new_code/f_mineos_kernels.m
@@ -1,19 +1,57 @@
-function [mode] = f_mineos_kernels(parm)
+function [SW_V_kernels] = f_mineos_kernels(parm, swperiods)
 
 % parameter_FRECHET_save(parm);  
 parameter_FRECHET
 % a1_run_mineos_check
 a2_mk_kernels
 
-%%% Do something here with FRECH_S and or FRECH_T. 
+% Get correct frechet variable. 
+if ( TYPE == 'S') 
+    frech = FRECH_S; 
+elseif ( TYPE == 'T')
+    frech = FRECH_T; 
+end 
 
-% a3_pull_dispersion
-% a4_pull_eigenfuncs
-% b1_run_idagrn
-% % a5_run_idagrn_branch % brb2024/05/22 I don't see this script in the Github repository. Maybe it is no longer needed. 
-% b2_run_idagrn_excitation
+% Parameter names 
+parmso = ["Z"  , "Vsv", "Vsh", "Vpv", "Vph", "eta", "rho"]; % Parameter names Zach's code
+parmsn = ["rad", "vsv", "vsh", "vpv", "vph", "eta", "rho"]; % Parameter names Josh's code
+n_per = length(frech); 
 
+% Initialize kernel cell array of structures. 
+SW_V_kernels = {}; 
+for ip = 1:n_per; 
+    SW_V_kernels{ip} = struct();
+end
 
+% Convert from Josh's kernel format to Zach's. 
+for iv = 1:length(parmso); % For each velocity-like parameter 
+    for ip = 1:n_per; % For each period
+
+        % Get input and output parameter names. 
+        outp = parmso(iv); % Output parameter, Zach's format. 
+        inp  = parmsn(iv); % Input parameter, Josh's format. 
+
+        % Extract input kernel value. If it is not there, it is zeros. 
+        if (TYPE=='T') && any(inp == ["vpv", "vph", "eta"]); % No kernel for Love vph, vph
+            ker = zeros( size(frech(ip).rad) ); 
+        else 
+            ker = frech(ip).(inp); % The kernel component we want 
+        end
+
+        % Convert between radius and depth.      Some notes: Zach's Z and Josh's rad have the same order (0 to 6371000). But the other kernel values are backwards (radius versus depth). Flip them. Zach's Z: 6370999 is last value. Last Vsv values are 0, top are not. Josh's rad: 6371000 is last value. Last Vsv values are non zero, first are 0. 
+        if ~ strcmp(outp, "Z"); 
+            ker = flip(ker); 
+        end 
+
+        % Assign input kernel to output
+        SW_V_kernels{ip}.(outp) = ker; 
+
+        % Insert period. TODO could do only if ip==n_per
+        per_out = [frech.per]'; 
+        SW_V_kernels{ip}.period = per_out(ip); 
+    end
+end
+SW_V_kernels = transpose(SW_V_kernels);
 
 
 end
--- transition_to_russell_mineos/new_code/f_mineos_run_phv_grv.m
+++ transition_to_russell_mineos/new_code/f_mineos_run_phv_grv.m
@@ -1,18 +1,9 @@
-function [mode] = f_mineos_run_phv_grv(parm)
+function [mode] = f_mineos_run_phv_grv(parm, swperiods)
 
-parameter_FRECHET_save(parm);  
+parameter_FRECHET_save(parm, swperiods);  
 parameter_FRECHET
 a1_run_mineos_check
 a2_mk_kernels
 a3_pull_dispersion
-%brb20240605 You can add a4 onward for eigenfunctions or synthetic
-%waveforms, but we were not using them. 
-% a4_pull_eigenfuncs
-% b1_run_idagrn
-% % a5_run_idagrn_branch % brb2024/05/22 I don't see this script in the Github repository. Maybe it is no longer needed. 
-% b2_run_idagrn_excitation
-
-
-
 
 end
